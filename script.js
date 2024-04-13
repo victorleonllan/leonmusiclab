3e91422fa16f31084c9f52ecc9a654cbf778642c
victor leon llanten <victor.leon.llanten@gmail.com>
2024-04-12 23:55:51 -0400

    Create Js file, add Jquery online, erase local Jquery. Iclean duplicated files.

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+  jQuery(document).ready(function ($) {
+    $(".slider-img").on("click", function () {
+      $(".slider-img").removeClass("active");
+      $(this).addClass("active");
+    });
+  });
